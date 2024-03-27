9e3aaae567571195b27db4d3c459783bc8c9f97c
孙成龙 <18715672252@163.com>
2024-03-27 11:01:30 +0800

    增加换肤按钮

--- src/renderer/src/components/NavBar.vue
+++ src/renderer/src/components/NavBar.vue
@@ -2,6 +2,7 @@
   <main class="text-slate-600 bg-gray-50 text-xs flex justify-between px-3 py-2 drag">
     <section class="opacity-80">孙成龙</section>
     <section class="flex justify-between gap-2">
+      <div class="nodrag cursor-pointer">换肤</div>
       <div>
         <router-link to="/" class="nodrag">转码</router-link>
       </div>
