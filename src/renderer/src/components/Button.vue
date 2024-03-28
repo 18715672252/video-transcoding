93af475250d47127dd53d194626ee8770228c723
孙成龙 <18715672252@163.com>
2024-03-28 16:07:33 +0800

    删除限制

--- src/renderer/src/components/Button.vue
+++ src/renderer/src/components/Button.vue
@@ -1,12 +1,31 @@
 <template>
   <main class="flex justify-center gap-2">
-    <el-button type="primary" size="small">停止转码</el-button>
-    <el-button type="danger" size="small">全部重置</el-button>
+    <el-button type="primary" size="small" @click="stop">停止转码</el-button>
+    <el-button type="danger" size="small" @click="delVideo">全部删除</el-button>
   </main>
 </template>
 
 <script setup lang="ts">
-
+import useCounterStore from '../store'
+import { storeToRefs } from 'pinia'
+import { ElMessage } from 'element-plus'
+const { delVideoNew } = useCounterStore()
+const { transcodingPogress } = storeToRefs(useCounterStore())
+const delVideo = () => {
+  console.log(transcodingPogress.value)
+  if (transcodingPogress.value) {
+    ElMessage.warning('正在转码，无法删除')
+    return
+  }
+  delVideoNew(true)
+}
+const stop = async () => {
+  if (!transcodingPogress.value) {
+    ElMessage.warning('暂无正在转码的视频')
+    return
+  }
+  await window.api.stopNotice()
+}
 </script>
 
 <style scoped>
